3fa653cc399a49b2aa181f81ae8793619ef868c1
jmarcooo <marco.odono@gmail.com>
2026-03-31 02:05:22 +0800

    Update firebase-setup.js

--- firebase-setup.js
+++ firebase-setup.js
@@ -1,4 +1,3 @@
-// firebase-setup.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
@@ -8,7 +7,6 @@ const firebaseConfig = {
   apiKey: "AIzaSyBt2fhVY8G0u0ET8ZpALcpMOcyPHlzAmFc",
   authDomain: "liga-ph.firebaseapp.com",
   projectId: "liga-ph",
-  // Fixed the bucket name to match your console exactly:
   storageBucket: "liga-ph.firebasestorage.app", 
   messagingSenderId: "114554829752",
   appId: "1:114554829752:web:4e0cea9f1b67f23f77ed4d",
