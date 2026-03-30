91f832a8eae846af02ffb903bfda77550791e1bf
jmarcooo <marco.odono@gmail.com>
2026-03-30 22:15:55 +0800

    Add metroManilaCities array to location.js

---
+++ location.js
@@ -0,0 +1,20 @@
+// locations.js
+export const metroManilaCities = [
+    "Caloocan City",
+    "Las Piñas City",
+    "Makati City",
+    "Malabon City",
+    "Mandaluyong City",
+    "Manila City",
+    "Marikina City",
+    "Muntinlupa City",
+    "Navotas City",
+    "Parañaque City",
+    "Pasay City",
+    "Pasig City",
+    "Municipality of Pateros",
+    "Quezon City",
+    "San Juan City",
+    "Taguig City",
+    "Valenzuela City"
+];
