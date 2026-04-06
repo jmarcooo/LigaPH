5ef5f90cf6c3f915d4d0431107d77504048a4971
jmarcooo <marco.odono@gmail.com>
2026-04-06 13:30:26 +0800

    Update nav-config.js

--- nav-config.js
+++ nav-config.js
@@ -3,31 +3,31 @@ export const navItems = [
         name: 'Home',
         icon: 'home',
         link: 'home.html',
-        activePaths: ['home.html', 'home', 'index.html', 'index', '/']
+        activePaths: ['home.html', 'index.html', '/']
     },
     {
         name: 'Feed',
         icon: 'dynamic_feed',
         link: 'feeds.html',
-        activePaths: ['feeds.html', 'feeds']
+        activePaths: ['feeds.html']
     },
     {
         name: 'Games',
         icon: 'sports_basketball',
         link: 'listings.html',
-        activePaths: ['listings.html', 'listings', 'game-details.html', 'game-details']
+        activePaths: ['listings.html', 'game-details.html']
     },
     {
         name: 'Squads',
         icon: 'shield',
         link: 'squads.html',
-        activePaths: ['squads.html', 'squads', 'squad-details.html', 'squad-details', 'leagues.html', 'leagues', 'league-details.html', 'league-details']
+        activePaths: ['squads.html', 'squad-details.html', 'leagues.html', 'league-details.html']
     },
     {
         name: 'Profile',
         icon: 'person',
         link: 'profile.html',
-        activePaths: ['profile.html', 'profile', 'edit-profile.html', 'edit-profile']
+        activePaths: ['profile.html', 'edit-profile.html']
     }
 ];
 
