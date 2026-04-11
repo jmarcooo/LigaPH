50063bd97a8914db881995a975ab04089039eb35
jmarcooo <marco.odono@gmail.com>
2026-04-11 09:50:05 +0800

    Create courts.js

---
+++ courts.js
@@ -0,0 +1,12 @@
+export const verifiedCourts = [
+    "Titan Love Court, BGC",
+    "Kerry Sports Manila, BGC",
+    "Gatorade Hoops Center, Mandaluyong",
+    "The Upper Deck, Pasig",
+    "Moro Lorenzo Sports Center, Quezon City",
+    "Ronac Art Center, San Juan",
+    "Cloverleaf Sports Arena, Makati",
+    "Filoil Flying V Centre, San Juan",
+    "Campbridge Hoops, Las Piñas",
+    "Homecourt, Pasig"
+];
