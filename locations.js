971fdc2f7ee152ec76d21790bb0fe84b1a3d876c
jmarcooo <marco.odono@gmail.com>
2026-04-13 09:59:11 +0800

    Update locations.js

--- locations.js
+++ locations.js
@@ -1,5 +1,5 @@
-// locations.js
-window.metroManilaCities = [
+// The Official 17 Local Government Units of Metro Manila
+export const metroManilaCities = [
     "Caloocan City",
     "Las Piñas City",
     "Makati City",
@@ -18,3 +18,70 @@ window.metroManilaCities = [
     "Taguig City",
     "Valenzuela City"
 ];
+
+// Verified Courts mapped strictly to the official LGUs above
+export const verifiedCourtsByCity = {
+    "Caloocan City": [],
+    "Las Piñas City": [
+        "Campbridge Hoops"
+    ],
+    "Makati City": [
+        "Cloverleaf Sports Arena",
+        "Phenom Sports Center",
+        "Gatorade Hoops Center (Makati)",
+        "Kings Court, Makati",
+        "Dasmarinas Village Clubhouse"
+    ],
+    "Malabon City": [],
+    "Mandaluyong City": [
+        "Gatorade Hoops Center, Shaw",
+        "Dumlao Sports Center",
+        "Wack Wack Golf & Country Club Gym",
+        "Greenfield District Pavilion"
+    ],
+    "Manila City": [
+        "San Andres Sports Complex",
+        "Rizal Memorial Coliseum",
+        "Ninoy Aquino Stadium"
+    ],
+    "Marikina City": [],
+    "Muntinlupa City": [
+        "De La Salle Zobel Sports Pavilion",
+        "San Beda Alabang Gym",
+        "Ayala Alabang Village Clubhouse"
+    ],
+    "Navotas City": [],
+    "Parañaque City": [],
+    "Pasay City": [
+        "Cuneta Astrodome",
+        "Mall of Asia Arena"
+    ],
+    "Pasig City": [
+        "The Upper Deck, Ortigas",
+        "Homecourt, Pasig",
+        "Ynares Sports Arena",
+        "Valle Verde 1 Clubhouse",
+        "PhilSports Arena"
+    ],
+    "Municipality of Pateros": [],
+    "Quezon City": [
+        "Moro Lorenzo Sports Center, Ateneo",
+        "Victoria Sports Tower",
+        "Amoranto Sports Complex",
+        "Celebrity Sports Plaza",
+        "Smart Araneta Coliseum"
+    ],
+    "San Juan City": [
+        "Ronac Art Center",
+        "Filoil Flying V Centre",
+        "Greenhills West Clubhouse"
+    ],
+    "Taguig City": [
+        "Titan Love Court, BGC",
+        "Kerry Sports Manila, BGC",
+        "Mckinley Hill Stadium",
+        "Tenement Court, Taguig",
+        "Treston International College Gym"
+    ],
+    "Valenzuela City": []
+};
